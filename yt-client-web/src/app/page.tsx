21459193e2a53a5e0c9dc0b5098e7dc71c70b9a7
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2023-10-01 18:25:42 +0100

    prettier installed and watch page created

--- yt-client-web/src/app/page.tsx
+++ yt-client-web/src/app/page.tsx
@@ -1,12 +1,11 @@
-
-import { ModeToggle } from '@/components/ModeToggle'
-import { Button } from '@/components/ui/Button'
-import Image from 'next/image'
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/Button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>
-      <ModeToggle />
-    </h1>
-  )
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
 }
