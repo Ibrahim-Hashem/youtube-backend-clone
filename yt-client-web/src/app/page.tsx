0b5ebc132ec0cdc702fac5376ed37f48ea8842f3
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2023-10-10 20:52:55 +0100

    implemented docker build function

--- yt-client-web/src/app/page.tsx
+++ yt-client-web/src/app/page.tsx
@@ -7,17 +7,21 @@ export default async function Home() {
 
   return (
     <main>
-      {videos.map((video) => (
-        <Link href={`/watch?v=${video.filename}`} key={video.id}>
-          <Image
-            src={"/thumbnail.png"}
-            alt="video"
-            width={120}
-            height={80}
-            className="m-10"
-          />
-        </Link>
-      ))}
+      <div className="flex flex-wrap ">
+        {videos.map((video) => (
+          <Link href={`/watch?v=${video.filename}`} key={video.id}>
+            <Image
+              src={"/thumbnail.png"}
+              alt="video"
+              width={120}
+              height={80}
+              className="m-10"
+            />
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
+
+export const revalidate = 30;
